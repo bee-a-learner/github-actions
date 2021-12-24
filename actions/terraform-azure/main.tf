9174cbc9dd43a444ba271bba1841876d372f6372
Rakesh Suryawanshi <rakesh.suryawanshi@avanade.com>
2021-12-24 13:37:28 +0000

    variables added

--- actions/terraform-azure/main.tf
+++ actions/terraform-azure/main.tf
@@ -11,7 +11,12 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.client_tenant_id
 }
+
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
---
+++ actions/terraform-azure/variables.tf
@@ -0,0 +1,5 @@
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "client_tenant_id" {}
