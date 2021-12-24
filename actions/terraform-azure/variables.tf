a4f863a13e5c3275a8520e3a96d5a2b68ed927e4
Rakesh Suryawanshi <rakesh.suryawanshi@avanade.com>
2021-12-24 13:53:54 +0000

    remote backend added

--- actions/terraform-azure/variables.tf
+++ actions/terraform-azure/variables.tf
@@ -1,5 +1,5 @@
 
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+# variable "subscription_id" {}
+# variable "client_id" {}
+# variable "client_secret" {}
+# variable "tenant_id" {}
