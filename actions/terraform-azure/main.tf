aad171a15342500f981e82de6a0a9a1adc229992
Rakesh Suryawanshi <rakesh.suryawanshi@avanade.com>
2021-12-24 13:34:59 +0000

    tf main.tf file formate corrected

--- actions/terraform-azure/main.tf
+++ actions/terraform-azure/main.tf
@@ -6,18 +6,11 @@ terraform {
     }
   }
 
-#    backend "azurerm" {
-#     resource_group_name  = "StorageAccount-ResourceGroup"
-#     storage_account_name = "abcd1234"
-#     container_name       = "tfstate"
-#     key                  = "prod.terraform.tfstate"
-#   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
 }
 
 # Create a resource group
